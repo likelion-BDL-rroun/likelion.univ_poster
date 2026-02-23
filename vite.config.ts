4d2432bbd157bc283a5b3f178ad8d6a3583e2197
김은지 <kim-eunji@BX-MacBook-Pro.local>
2026-02-23 17:29:09 +0900

    fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/likelion.univ_poster/',
+  base: './',
   build: {
     outDir: 'docs',
     emptyOutDir: true,
