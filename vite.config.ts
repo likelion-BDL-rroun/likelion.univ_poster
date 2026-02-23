d33c00d09e2a5bf4dde88147c5c916313f96353e
김은지 <kim-eunji@BX-MacBook-Pro.local>
2026-02-23 17:05:26 +0900

    deploy github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/likelion.univ_poster/',
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true,
+  },
   plugins: [
     // The React and Tailwind plugins are both required for Make, even if
     // Tailwind is not being actively used – do not remove them
